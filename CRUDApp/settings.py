0306322caa908f5e3ed593d9ea86cc36bfacb8d3
Reema Kasula <reemashrestha2019@gmail.com>
2026-02-07 21:33:50 +0545

    Change database password in settings.py

--- CRUDApp/settings.py
+++ CRUDApp/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'todoapp_db',              
         'USER': 'postgres',                 
-        'PASSWORD': '613jm7',         
+        'PASSWORD': '777',         
         'HOST': 'localhost',                
         'PORT': '5432',                      
     }
